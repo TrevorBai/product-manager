515058995442c5380479f3255ce37cb8d4162da1
Trevor Bai <baijin2011@gmail.com>
2019-12-19 08:56:38 -0500

    replacing useContext with a custom hook

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -4,7 +4,7 @@ let globalState = {}
 let listeners = []
 let actions = {}
 
-export const useStore = () => {
+export const useStore = (shouldListen = true) => {
   const setState = useState(globalState)[1]
 
   const dispatch = (actionIdentifier, payload) => {
@@ -14,16 +14,19 @@ export const useStore = () => {
     for (const listener of listeners) {
       listener(globalState)
     }
-
   }
 
   useEffect(() => {
-    listeners.push(setState)
+    if (shouldListen) {
+      listeners.push(setState)
+    }
 
     return () => {
-      listeners = listeners.filter(listener => listener !== setState)
+      if (shouldListen) {
+        listeners = listeners.filter(listener => listener !== setState)
+      }
     }
-  }, [setState])
+  }, [setState, shouldListen])
 
   return [globalState, dispatch]
 }
